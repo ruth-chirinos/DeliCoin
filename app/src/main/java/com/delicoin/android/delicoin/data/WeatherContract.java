03652b588841099688ae2713af78fcb8fe4d9bd7
Ruth Chirinos <ruth.chirinos@gmail.com>
2015-10-03 20:49:32 -0400

    Integration DeliCoin, part 2

--- app/src/main/java/com/delicoin/android/delicoin/data/WeatherContract.java
+++ app/src/main/java/com/delicoin/android/delicoin/data/WeatherContract.java
@@ -19,14 +19,14 @@ public class WeatherContract {
     // relationship between a domain name and its website.  A convenient string to use for the
     // content authority is the package name for the app, which is guaranteed to be unique on the
     // device.
-    public static final String CONTENT_AUTHORITY = "com.example.android.sunshine.app";
+    public static final String CONTENT_AUTHORITY = "com.delicoin.android.delicoin";
 
     // Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
     // the content provider.
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
     // Possible paths (appended to base content URI for possible URI's)
-    // For instance, content://com.example.android.sunshine.app/weather/ is a valid path for
+    // For instance, content://com.delicoin.android.delicoin/weather/ is a valid path for
     // looking at weather data. content://com.example.android.sunshine.app/givemeroot/ will fail,
     // as the ContentProvider hasn't been given any information on what to do with "givemeroot".
     // At least, let's hope not.  Don't be that dev, reader.  Don't be that dev.
